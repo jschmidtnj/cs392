5d8adf717024d5cc78b7dcfdd30ab30c9b8d3470
Joshua <jschmidtnj@gmail.com>
2020-04-03 16:38:31 -0400

    added correct parsing of input str

--- assignments/minishell/minishell.c
+++ assignments/minishell/minishell.c
@@ -39,6 +39,70 @@ int print_prompt() {
   return EXIT_SUCCESS;
 }
 
+void print_malloc_failed() {
+  fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
+}
+
+struct process_str_output {
+  int exit_status;
+  char ** arguments;
+  int num_arguments;
+};
+
+struct process_str_output process_input(const char * input_str, const int * input_str_len) {
+  struct process_str_output res;
+  int num_args = 0;
+  char ** args;
+  int current_arg_index = 0;
+  for (int i = 0; i < 2; i++) {
+    int current_parse_index = 0;
+    while (current_parse_index < *input_str_len) {
+      int arg_len = 0;
+      int start_index = current_parse_index;
+      while (current_parse_index < *input_str_len && *(input_str + current_parse_index) != ' ') {
+        arg_len++;
+        current_parse_index++;
+      }
+      if (arg_len > 0) {
+        if (i == 0) {
+          num_args++;
+        } else {
+          char * current_str;
+          if ((current_str = (char *)malloc(sizeof(char) * (arg_len + 1))) == NULL) {
+            print_malloc_failed();
+            for (int i = 0; i < num_args; i++) {
+              free(args + i);
+            }
+            free(args);
+            res.exit_status = EXIT_FAILURE;
+            return res;
+          }
+          memcpy(current_str, input_str + start_index, arg_len);
+          *(current_str + arg_len) = '\0';
+          *(args + current_arg_index) = current_str;
+          current_arg_index++;
+        }
+      }
+      while (current_parse_index < *input_str_len && *(input_str + current_parse_index) == ' ') {
+        current_parse_index++;
+      }
+    }
+    if (i == 0) {
+      if ((args = (char **)malloc(sizeof(char *) * (num_args + 1))) == NULL) {
+        print_malloc_failed();
+        res.exit_status = EXIT_FAILURE;
+        return res;
+      }
+    } else {
+      *(args + num_args) = NULL;
+    }
+  }
+  res.exit_status = EXIT_SUCCESS;
+  res.arguments = args;
+  res.num_arguments = num_args;
+  return res;
+}
+
 int main() {
   struct sigaction action;
   memset(&action, '\0', sizeof(struct sigaction));
@@ -51,7 +115,7 @@ int main() {
   }
 
   size_t max_promt_size = ARG_MAX;
-  char *prompt_input = (char *)malloc(ARG_MAX);
+  char *prompt_input = (char *)malloc(sizeof(char) * ARG_MAX);
   if (prompt_input == NULL) {
     fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
     return EXIT_FAILURE;
@@ -79,7 +143,17 @@ int main() {
       pid_t pid;
       if ((pid = fork()) == 0) {
         // in child process
-        if (execlp(prompt_input, prompt_input, NULL) == -1) {
+        int input_len = (int)prompt_input_len;
+        struct process_str_output process_res = process_input(prompt_input, &input_len);
+        if (process_res.exit_status != EXIT_SUCCESS) {
+          goto CLEANUP_FAILURE;
+        }
+        int exec_status = execvp(process_res.arguments[0], process_res.arguments);
+        for (int i = 0; i < process_res.num_arguments; i++) {
+          free(process_res.arguments[i]);
+        }
+        free(process_res.arguments);
+        if (exec_status == -1) {
           fprintf(stderr, "Error: exec() failed. %s\n", strerror(errno));
           goto CLEANUP_FAILURE;
         }
