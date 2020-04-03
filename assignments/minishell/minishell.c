594aeaa46ff0725b7f7df608b93c1412d875e2e1
Joshua <jschmidtnj@gmail.com>
2020-04-03 18:34:38 -0400

    added minishell solution

--- assignments/minishell/minishell.c
+++ assignments/minishell/minishell.c
@@ -5,21 +5,27 @@
  * Pledge : I pledge my honor that I have abided by the Stevens Honor System.
  * Description : minishell
  ******************************************************************************/
+
 #define _GNU_SOURCE
 
 #include <errno.h>
 #include <linux/limits.h>
+#include <pwd.h>
 #include <setjmp.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #define BRIGHT_BLUE "\x1b[34;1m"
 #define DEFAULT_COLOR "\x1b[0m"
+#define ERROR_COLOR "\x1b[0m"
+
+#define EXIT_WARNING -1
 
 sigjmp_buf jmp_prompt;
 
@@ -31,8 +37,8 @@ void catch_signal() {
 int print_prompt() {
   char cwd[PATH_MAX];
   if ((getcwd(cwd, PATH_MAX) == NULL)) {
-    fprintf(stderr, "Error: Cannot get current working directory. %s\n",
-            strerror(errno));
+    fprintf(stderr, "%sError: Cannot get current working directory. %s\n",
+            ERROR_COLOR, strerror(errno));
     return EXIT_FAILURE;
   }
   printf("%s[%s%s%s]$ ", DEFAULT_COLOR, BRIGHT_BLUE, cwd, DEFAULT_COLOR);
@@ -40,26 +46,34 @@ int print_prompt() {
 }
 
 void print_malloc_failed() {
-  fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
+  fprintf(stderr, "%sError: malloc() failed. %s.\n", ERROR_COLOR,
+          strerror(errno));
 }
 
-struct process_str_output {
+void print_change_dir_failed(const char *dir) {
+  fprintf(stderr, "%sCannot change directory to '%s'. %s.\n", ERROR_COLOR, dir,
+          strerror(errno));
+}
+
+struct process_input_str_res {
   int exit_status;
-  char ** arguments;
+  char **arguments;
   int num_arguments;
 };
 
-struct process_str_output process_input(const char * input_str, const int * input_str_len) {
-  struct process_str_output res;
+struct process_input_str_res process_input(const char *input_str,
+                                           const int *input_str_len) {
+  struct process_input_str_res res;
   int num_args = 0;
-  char ** args;
+  char **args;
   int current_arg_index = 0;
   for (int i = 0; i < 2; i++) {
     int current_parse_index = 0;
     while (current_parse_index < *input_str_len) {
       int arg_len = 0;
       int start_index = current_parse_index;
-      while (current_parse_index < *input_str_len && *(input_str + current_parse_index) != ' ') {
+      while (current_parse_index < *input_str_len &&
+             *(input_str + current_parse_index) != ' ') {
         arg_len++;
         current_parse_index++;
       }
@@ -67,8 +81,9 @@ struct process_str_output process_input(const char * input_str, const int * inpu
         if (i == 0) {
           num_args++;
         } else {
-          char * current_str;
-          if ((current_str = (char *)malloc(sizeof(char) * (arg_len + 1))) == NULL) {
+          char *current_str;
+          if ((current_str = (char *)malloc(sizeof(char) * (arg_len + 1))) ==
+              NULL) {
             print_malloc_failed();
             for (int i = 0; i < num_args; i++) {
               free(args + i);
@@ -83,7 +98,8 @@ struct process_str_output process_input(const char * input_str, const int * inpu
           current_arg_index++;
         }
       }
-      while (current_parse_index < *input_str_len && *(input_str + current_parse_index) == ' ') {
+      while (current_parse_index < *input_str_len &&
+             *(input_str + current_parse_index) == ' ') {
         current_parse_index++;
       }
     }
@@ -103,33 +119,123 @@ struct process_str_output process_input(const char * input_str, const int * inpu
   return res;
 }
 
+struct process_cd_res {
+  int exit_status;
+  char *argument;
+};
+
+struct process_cd_res get_cd_arg(const char *full_args, const int len) {
+  struct process_cd_res res;
+  int current_index = 0;
+  // handle cd
+  while (*(full_args + current_index) != ' ') {
+    current_index++;
+  }
+  // handle spaces
+  while (*(full_args + current_index) == ' ') {
+    current_index++;
+  }
+  if (*(full_args + current_index) == '~') {
+    current_index++;
+    for (; current_index < len; current_index++) {
+      if (*(full_args + current_index) != ' ') {
+        fprintf(stderr, "%sError: Too many arguments found for cd.\n",
+                ERROR_COLOR);
+        res.exit_status = EXIT_WARNING;
+        return res;
+      }
+    }
+    // home directory
+    if ((res.argument = (char *)malloc(sizeof(char) * (2))) == NULL) {
+      print_malloc_failed();
+      res.exit_status = EXIT_FAILURE;
+      return res;
+    }
+    *(res.argument) = '~';
+    *(res.argument + 1) = '\0';
+    res.exit_status = EXIT_SUCCESS;
+    return res;
+  } else if (*(full_args + current_index) != '"') {
+    fprintf(stderr, "%sError: No first parenthesis found for cd.\n",
+            ERROR_COLOR);
+    res.exit_status = EXIT_WARNING;
+    return res;
+  }
+  current_index++;
+  int start_index = current_index;
+  int arg_len = 0;
+  bool last_escape = false;
+  bool found_closing = false;
+  for (; current_index < len; current_index++) {
+    if (*(full_args + current_index) == '\\') {
+      arg_len++;
+      last_escape = true;
+      continue;
+    } else {
+      last_escape = false;
+    }
+    if (*(full_args + current_index) == '"' && !last_escape) {
+      found_closing = true;
+      break;
+    } else {
+      arg_len++;
+    }
+  }
+  if (!found_closing) {
+    fprintf(stderr, "%sError: No closing parenthesis found for cd.\n",
+            ERROR_COLOR);
+    res.exit_status = EXIT_WARNING;
+    return res;
+  }
+  current_index++;
+  for (; current_index < len; current_index++) {
+    if (*(full_args + current_index) != ' ') {
+      fprintf(stderr, "%sError: Too many arguments found for cd.\n",
+              ERROR_COLOR);
+      res.exit_status = EXIT_WARNING;
+      return res;
+    }
+  }
+  if ((res.argument = (char *)malloc(sizeof(char) * (arg_len + 1))) == NULL) {
+    print_malloc_failed();
+    res.exit_status = EXIT_FAILURE;
+    return res;
+  }
+  memcpy(res.argument, full_args + start_index, arg_len);
+  *(res.argument + arg_len) = '\0';
+  res.exit_status = EXIT_SUCCESS;
+  return res;
+}
+
 int main() {
   struct sigaction action;
   memset(&action, '\0', sizeof(struct sigaction));
   action.sa_handler = catch_signal;
   action.sa_flags = SA_RESTART;
   if (sigaction(SIGINT, &action, NULL) == -1) {
-    fprintf(stderr, "Error: Cannot register signal handler. %s.\n",
-            strerror(errno));
+    fprintf(stderr, "%sError: Cannot register signal handler. %s.\n",
+            ERROR_COLOR, strerror(errno));
     return EXIT_FAILURE;
   }
 
-  size_t max_promt_size = ARG_MAX;
+  size_t max_prompt_size = ARG_MAX;
   char *prompt_input = (char *)malloc(sizeof(char) * ARG_MAX);
   if (prompt_input == NULL) {
-    fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
+    print_malloc_failed();
     return EXIT_FAILURE;
   }
 
   sigsetjmp(jmp_prompt, 1);
+  ssize_t prompt_input_len;
   while (true) {
     if (print_prompt() == EXIT_FAILURE) {
       goto CLEANUP_FAILURE;
     }
-    ssize_t prompt_input_len;
-    if ((prompt_input_len = getline(&prompt_input, &max_promt_size, stdin)) ==
+    if ((prompt_input_len = getline(&prompt_input, &max_prompt_size, stdin)) ==
         -1) {
-      fprintf(stderr, "Error: problem with getline. %s\n", strerror(errno));
+      fprintf(stderr, "%sError: Problem with getline. %s\n", ERROR_COLOR,
+              strerror(errno));
+      goto CLEANUP_FAILURE;
     }
     if (*(prompt_input + prompt_input_len - 1) == '\n') {
       *(prompt_input + prompt_input_len - 1) = '\0';
@@ -139,22 +245,49 @@ int main() {
       continue;
     } else if (strcmp(prompt_input, "exit") == 0) {
       goto CLEANUP_SUCCESS;
+    } else if (strncmp(prompt_input, "cd", 2) == 0) {
+      uid_t uid = getuid();
+      struct passwd *pwuid;
+      if ((pwuid = getpwuid(uid)) == NULL) {
+        fprintf(stderr, "%sCannot get passwd entry. %s\n", ERROR_COLOR,
+                strerror(errno));
+        goto CLEANUP_FAILURE;
+      }
+      struct process_cd_res cd_process_res =
+          get_cd_arg(prompt_input, prompt_input_len);
+      if (cd_process_res.exit_status == EXIT_FAILURE) {
+        goto CLEANUP_FAILURE;
+      } else if (cd_process_res.exit_status == EXIT_SUCCESS) {
+        if (strcmp(cd_process_res.argument, "~") == 0) {
+          if (chdir(pwuid->pw_dir) == -1) {
+            print_change_dir_failed(cd_process_res.argument);
+          }
+        } else {
+          if (chdir(cd_process_res.argument) == -1) {
+            print_change_dir_failed(cd_process_res.argument);
+          }
+        }
+        free(cd_process_res.argument);
+      }
     } else {
       pid_t pid;
       if ((pid = fork()) == 0) {
         // in child process
         int input_len = (int)prompt_input_len;
-        struct process_str_output process_res = process_input(prompt_input, &input_len);
+        struct process_input_str_res process_res =
+            process_input(prompt_input, &input_len);
         if (process_res.exit_status != EXIT_SUCCESS) {
           goto CLEANUP_FAILURE;
         }
-        int exec_status = execvp(process_res.arguments[0], process_res.arguments);
+        int exec_status =
+            execvp(process_res.arguments[0], process_res.arguments);
         for (int i = 0; i < process_res.num_arguments; i++) {
           free(process_res.arguments[i]);
         }
         free(process_res.arguments);
         if (exec_status == -1) {
-          fprintf(stderr, "Error: exec() failed. %s\n", strerror(errno));
+          fprintf(stderr, "%sError: exec() failed. %s\n", ERROR_COLOR,
+                  strerror(errno));
           goto CLEANUP_FAILURE;
         }
       } else if (pid > 0) {
@@ -162,14 +295,16 @@ int main() {
         int status;
         // wait for exec to finish
         if (wait(&status) == -1) {
-          fprintf(stderr, "Error: wait() failed. %s\n", strerror(errno));
+          fprintf(stderr, "%sError: wait() failed. %s\n", ERROR_COLOR,
+                  strerror(errno));
           goto CLEANUP_FAILURE;
         }
         if (WEXITSTATUS(status) != EXIT_SUCCESS) {
           goto CLEANUP_FAILURE;
         }
       } else {
-        fprintf(stderr, "Error: fork() failed. %s\n", strerror(errno));
+        fprintf(stderr, "%sError: fork() failed. %s\n", ERROR_COLOR,
+                strerror(errno));
         goto CLEANUP_FAILURE;
       }
     }
